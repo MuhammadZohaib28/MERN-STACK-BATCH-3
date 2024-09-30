f54ce0730692fed8441a381d6d068ecaa11d778a
Muhammad Zohaib <muhammad.zohaib@miletap.com>
2024-09-30 21:51:11 +0500

    JavaScript Day 1

---
+++ JavaScript/day1.js
@@ -0,0 +1,36 @@
+console.log("123, Hello World");
+
+
+// DATA TYPES
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. Object
+// 7. Symbol
+// 8. Array
+
+
+// VARIABLES
+
+
+    // var - keyword
+    //identifier - name of variable
+
+
+var userName = "Adil Khan";
+var userAge = 21;
+
+console.log(`The name of user is ${userName} and the user's age is ${userAge}`)
+
+
+var hobby1 = "Sketching";
+var hobby2 = "Palm Reading";
+var hobby3 = "Playing";
+var username = prompt("Enter your name");
+
+
+console.log(`User ka naaam hai  ${username} aur user ki hobbies hain ${hobby1}, ${hobby2}, ${hobby3}.`)
+
+
