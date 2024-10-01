d6dbb2f239ec7ff493e29c812d99e677ecae4e5f
Muhammad Zohaib <muhammad.zohaib@miletap.com>
2024-10-01 21:50:05 +0500

    DAY 2 JAVASCRIPT

---
+++ JavaScript/day2.js
@@ -0,0 +1,96 @@
+// CONSTANT  and  LET  Keyword
+
+var userage = 23;
+
+// console.log(userage, "YEH USER KI AGE HAI")
+
+console.log(`${userage} YEH USER KI AGE HAI`)
+
+console.log(typeof userage, "YEH userage KI TYPE HAI")
+
+
+
+//GLOBAL SCOPE - var
+
+//BLOCK SCOPE - let, const
+
+
+var userName = "Salman";
+var userName = "Adil";
+
+console.log(userName, "YEH USER KI NAME HAI")
+
+
+
+const schoolName = "Al Shams"
+const schoolNamee = "Gallant Foundation"
+
+// console.log(schoolNamee)
+
+
+// let keyword
+
+
+{
+    var schoolAdress = "Karachi"
+    console.log(schoolAdress, "YEH BLOCK K ANDAR HAI")
+}
+
+
+
+console.log(schoolAdress, "YEH BLOCK K BAHAR HAI")
+
+
+
+// IF ELSE CONDITION
+
+const nadraAge = 16;
+
+
+
+if (nadraAge >= 18) {
+    console.log("APP KA CNIC BAN SAKTA HAI")
+} else {
+    console.log("APP KA CNIC NAHI BAN SAKTA")
+}
+
+
+
+
+let userPunch = 1200;
+
+if (userPunch >= 1100) {
+    console.log("YOU ARE WINNER ")
+} else {
+    console.log("TRY AGAIN")
+}
+
+
+const userageee = "23";
+
+const newAge = userageee + 1;
+
+
+console.log(userageee, "YEH USER KI PURANI AGE HAI")
+console.log(newAge, "YEH USER KI NEW AGE HAI")
+
+
+
+
+
+
+let isRaining = false;
+
+console.log(typeof isRaining)
+
+if (isRaining) {
+    console.log("CLASS NAHI HOGIII")
+} else {
+    console.log("CLASS HOGI")
+}
+
+
+
+// = - for assignment
+// == - for comparison
+// ===
